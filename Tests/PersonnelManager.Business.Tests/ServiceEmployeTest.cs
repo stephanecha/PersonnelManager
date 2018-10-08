64714d078ca27508cab541ad28e73d0eef2fc4ef
Yannick BAZAN <ybazan.pro@live.fr>
2018-10-08 15:34:30 +0200

    Ecriture d'un test pour la date d'embauche

--- Tests/PersonnelManager.Business.Tests/ServiceEmployeTest.cs
+++ Tests/PersonnelManager.Business.Tests/ServiceEmployeTest.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonnelManager.Business.Exceptions;
+using PersonnelManager.Business.Services;
+using PersonnelManager.Dal.Data;
 using PersonnelManager.Dal.Entites;
 
 namespace PersonnelManager.Business.Tests
@@ -11,8 +14,33 @@ namespace PersonnelManager.Business.Tests
     public class ServiceEmployeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValiderNomEtPrenomRequis()
         {
+            Assert.IsTrue(
+                TestsHelper.HasAttribute<Employe, RequiredAttribute>(x => x.Nom));
+            Assert.IsTrue(
+                TestsHelper.HasAttribute<Employe, RequiredAttribute>(x => x.Prenom));
+        }
+
+        [TestMethod]
+        public void DateEmbauchePosterieureA1920()
+        {
+            var serviceEmploye = new ServiceEmploye(new DbDataEmploye());
+            var ouvrier = new Ouvrier
+            {
+                Nom = "Dupont",
+                Prenom = "Gérard",
+                DateEmbauche = new DateTime(1919, 12, 31),
+                TauxHoraire = 12
+            };
+
+            var exception = Assert.ThrowsException<BusinessException>(() =>
+            {
+                serviceEmploye.EnregistrerOuvrier(ouvrier);
+            });
+
+            Assert.AreEqual("La date d'embauche doit être > 1920", 
+                exception.Message);
         }
     }
 }
